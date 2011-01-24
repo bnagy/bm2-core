43a4d64dccacebeab3debb945dfe298894a70e01
Ben Nagy <github@ben.iagu.net>
2011-01-24 13:42:50 +0545

    change disassembly algorithm

--- detail_parser.rb
+++ detail_parser.rb
@@ -56,8 +56,8 @@ module DetailParser
     # Out: [[0, "316c5a0e mov eax,dword ptr [eax]"], [1, 
     def self.disassembly( detail_string )
         # This is tied to the current debugger output style, which is a bit crap
-        # it assumes that u@eip is run just before the !exploitable command.
-        instructions=detail_string.match(/:\n(.*?)^IDENT/m)[1].split("\n")
+        # it assumes that kv and u@eip is run just before the !exploitable command.
+        instructions=detail_string.match(/ChildEBP.*:\n(.*?)^IDENT/m)[1].split("\n")
         #instructions=detail_string.scan( /BASIC_BLOCK_INSTRUCTION:(.*)$/ ).flatten
         (0..instructions.length-1).to_a.zip instructions
     rescue
